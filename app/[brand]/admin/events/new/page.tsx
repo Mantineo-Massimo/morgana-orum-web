42140bd53f3d18e00eb667eb1e3e8cc8a7e58e78
Mantineo-Massimo <mantineomassimo2003@gmail.com>
2026-02-19 23:20:11 +0100

    Risolto errore build next pre-rendering su pagine new

--- app/[brand]/admin/events/new/page.tsx
+++ app/[brand]/admin/events/new/page.tsx
@@ -1,5 +1,7 @@
 import EventForm from "@/components/admin/event-form"
 
+export const dynamic = "force-dynamic"
+
 export default function NewEventPage({ params }: { params: { brand: string } }) {
     return <EventForm brand={params.brand} />
 }
--- app/[brand]/admin/representatives/new/page.tsx
+++ app/[brand]/admin/representatives/new/page.tsx
@@ -1,5 +1,7 @@
 import RepresentativeForm from "@/components/admin/representative-form"
 
+export const dynamic = "force-dynamic"
+
 export default function NewRepresentativePage({ params }: { params: { brand: string } }) {
     return <RepresentativeForm brand={params.brand} />
 }
